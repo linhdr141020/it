3ebaf214e1f157d55f8fae9c489b8cb1a81f0155
linhdr141020 <81572858+linhdr141020@users.noreply.github.com>
2022-08-06 14:40:31 +0700

    middleware

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
+import morgan from 'morgan'
 import configViewEngine from './configs/viewEngine';
 import initWebRoute from './route/web'
 import initApiRoute from './route/api'
 // import connection from './configs/connectDB'
 
 require('dotenv').config()
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+
+app.use((req,res,next) => {
+  console.log(req.method);
+  next();
+})
 
 const port = process.env.PORT || 3001;
 //setup view engine
@@ -16,6 +23,11 @@ configViewEngine(app)
 
 //init web route
 initWebRoute(app);
+
+//handle 404 not found
+app.use((req,res) => {
+  return res.render('404.ejs')
+})
 
 initApiRoute(app)
 
