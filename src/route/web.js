159f3006d3784203dc16768277bce635bf497c76
linhdr141020 <81572858+linhdr141020@users.noreply.github.com>
2022-08-06 13:35:59 +0700

    upload file

--- src/route/web.js
+++ src/route/web.js
@@ -1,7 +1,32 @@
 import express from "express";
 import homeController from '../controller/homeController'
+import multer from 'multer'
+import path from 'path'
+var appRoot = require('app-root-path')
 
 let route = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      cb(null, appRoot + "/src/public/image/");
+  },
+
+  // By default, multer removes file extensions so let's add them back
+  filename: function(req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const imageFilter = function(req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+      req.fileValidationError = 'Only image files are allowed!';
+      return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
+
+let upload = multer({storage: storage,fileFilter: imageFilter});
 
 const initWebRoute = (app) => {
   route.get("/", homeController.getHomePage);
@@ -10,6 +35,9 @@ const initWebRoute = (app) => {
   route.post('/delete-user',homeController.deleteUser)
   route.get('/edit-user/:id',homeController.getEditPage)
   route.post('/update-user',homeController.postUpdateUser)
+  route.get('/upload',homeController.getUploadFilePage)
+  route.post('/upload-profile-pic',upload.single('profile_pic',homeController),homeController.handleUploadFile)
+
   return app.use("/", route);
 };
 module.exports = initWebRoute;
